e64c78bad715e20b5097169e34753b8468b8a9c7
xudongyi <1922448115@qq.com>
2018-04-03 14:01:45 +0800

    前后端框架

---
+++ src/main/wechat_front/src/page/home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <a href="javascript:;"@click="test">测试123123</a>
+      <img src="../images/personal/1-lx.png">
+    </div>
+</template>
+<script>
+    export default{
+        data(){
+            return {}
+        },
+        created() {
+
+        },
+        mounted() {
+
+        },
+        methods: {
+            test:function(){
+                alert("测试");
+            }
+        },
+        components: {
+
+        }
+    }
+</script>
+<style scoped>
+
+</style>
